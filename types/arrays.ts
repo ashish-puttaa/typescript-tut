1bc5e511fb392619acf5989a092e1b60ca2e3bf3
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-09-15 20:57:23 +0530

    core types - Array

---
+++ types/arrays.ts
@@ -0,0 +1,19 @@
+/* Explicitly declare Array type by suffixing '[]' to a basic type */
+
+const numbers: number[] = [1, 2, 3];
+
+let favouriteActivities: string[];
+favouriteActivities = ['sport'];
+
+/* Here person.hobbies is of type string[] */
+
+const person = {
+   name: 'Ash',
+   age: 20,
+   hobbies: ['Sports', 'Cooking'],
+};
+
+for (const hobby of person.hobbies) {
+   console.log(hobby.toUpperCase());
+   /* console.log(hobby.map()); // !! Compile Error !! */
+}
--- types/core-types.ts
+++ types/core-types.ts
@@ -3,6 +3,8 @@
    1. number
    2. string
    3. boolean
+   4. object
+   5. array
 */
 
 function add(num1: number, num2: number, printResult: boolean, resultPhrase: string) {
