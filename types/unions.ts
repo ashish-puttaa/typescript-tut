0c4993728132d9a67a9a4396fb4ca667c3e985ba
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-09-17 20:31:14 +0530

    core types - Union

---
+++ types/unions.ts
@@ -0,0 +1,22 @@
+/* 
+   Union types are used to declare more than one data type for a variable or a function parameter. 
+   The pipe symbol '|' is used to union two types.
+*/
+
+function combine(input1: number | string, input2: number | string) {
+   let result;
+
+   if (typeof input1 === 'number' && typeof input2 === 'number') {
+      result = input1 + input2;
+   } else {
+      result = input1.toString() + input2.toString();
+   }
+
+   return result;
+}
+
+const combinedPrices = combine(2.5, 1.75);
+console.log(combinedPrices);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
