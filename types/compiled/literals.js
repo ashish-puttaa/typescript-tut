700884d632dbab2c2d1d5bad128a23e4daf9ab1b
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-09-17 21:02:58 +0530

    core types - Literal

---
+++ types/compiled/literals.js
@@ -0,0 +1,22 @@
+/*
+   Literal types are used to declare an exact value which a string, number, or boolean must have.
+*/
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultConversion === 'as-number')
+        return +result;
+    else
+        return result.toString();
+}
+var combinedPrices = combine(2.5, 1.75, 'as-number');
+console.log(combinedPrices);
+var combinedStringPrices = combine(2.5, 1.75, 'as-number');
+console.log(combinedStringPrices);
+var combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
