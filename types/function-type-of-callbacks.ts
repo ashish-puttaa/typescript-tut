b07f5b3cef6da8969d5b649c8e1e53cf8a91b7e1
ashish-puttaa <ashishputtaa.98@gmail.com>
2020-09-19 20:21:57 +0530

    function type of callbacks

---
+++ types/function-type-of-callbacks.ts
@@ -0,0 +1,23 @@
+/*
+   You can use the arrow function notation to specifiy function types of callbacks
+*/
+
+function addAndHandle(num1: number, num2: number, cb: (num: number) => void) {
+   const result = num1 + num2;
+   cb(result);
+}
+
+/* This works */
+
+addAndHandle(5, 10, (result) => console.log(result));
+
+/*
+   This does not work
+
+   addAndHandle(5, 10, (result, i) => console.log(result, i))
+*/
+
+/* 
+   In the case of callbacks, the 'void' return type does not mean that the cb wont return anything.
+   It means that the function does not do anything with return value of the callback
+*/
